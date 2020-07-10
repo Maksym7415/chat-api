77c3579b8e5b76ac31e0121e3d2f317af3c19af7
Maksym7415 <popovmaksim7415@gmail.com>
2020-07-10 15:11:49 +0300

    deleted comments in mailsender

--- src/helpers/nodeMailer.js
+++ src/helpers/nodeMailer.js
@@ -1,25 +1,22 @@
 const nodemailer = require("nodemailer");
 
 async function handleSendEmail(to, text) {
-   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
-    secure: false, // true for 465, false for other ports
+    secure: false,
     service: 'gmail',
     auth: {
-      user: 'mynamesurnamemyname@gmail.com', // generated ethereal user
-      pass: 'aA123456!', // generated ethereal password
+      user: 'mynamesurnamemyname@gmail.com',
+      pass: 'aA123456!',
     },
   });
 
-  // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: 'Conversation APP', // sender address
-    to, // list of receivers
-    subject: "Confirmation code", // Subject line
-    text, // plain text body
-    // html: "<b>Hello world?</b>", // html body
+    from: 'Conversation APP',
+    to,
+    subject: "Confirmation code",
+    text, 
   });
 }
 
