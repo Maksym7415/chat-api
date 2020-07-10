ad67e91b93cff88a0179115247d2a8c565ba905f
Alesha <VAlesha@hq.telesens.lan>
2020-07-10 18:36:37 +0300

    some change in api

--- src/api/user.api/user.controller.js
+++ src/api/user.api/user.controller.js
@@ -1,16 +1,16 @@
 const models = require('../../../models');
+const handleSendEmail = require('../../helpers/nodeMailer');
 
 module.exports = {
   signUp: async (req, res) => {
-    const { firstName, lastName, login } = req.body;
     try {
+      const { firstName, lastName, login } = req.body;
       const isUser = await models.User.findOne({ where: { login } });
-      console.log(isUser);
       if (!isUser) {
         const newUser = await models.User.create({
           firstName, lastName, login, status: 'free',
         });
-        res.json({ data: newUser, status: 200, message: 'registration successful' });
+        res.json({ data: { email: newUser.login }, status: 200, message: 'registration successful' });
       }
       res.json({ status: 400, message: 'such login already used in the system' });
     } catch (e) {
@@ -18,18 +18,39 @@ module.exports = {
     }
   },
   signIn: async (req, res) => {
-    const { login } = req.body;
     try {
-      const isUser = await models.User.findOne({ where: { login } });
+      const { login } = req.body;
+
+      const verificationCode = Math.floor(Math.random() * 100000);
+      const [isUser] = await models.User.update({ verificationCode }, {
+        where: { login },
+      });
+      console.log(login, verificationCode, isUser);
       if (isUser) {
+        try {
+          await handleSendEmail(login, `${verificationCode}`);
+          res.json({ status: 200, message: 'send you your verification code' });
+        } catch (e) {
+          res.json({ status: 400, message: 'some problems with code transfer' });
+        }
         res.json({ data: isUser, status: 200, message: 'checkEmail' });
       }
-      res.json({ status: 400, message: 'you need to registrate your account' });
+      res.json({ status: 400, message: 'you need to registrate your account', code: 999 });
     } catch (e) {
       res.json({ status: 501, message: e });
     }
   },
-  // sendCheckCode: async (req, res) => {
-
-  // },
+  sendCheckCode: async (req, res) => {
+    try {
+      const { verificationCode, login } = req.body;
+      const isSuccess = await models.User.findOne({ where: { login, verificationCode } });
+      console.log(isSuccess);
+      if (isSuccess) {
+        return res.json({ status: 200, message: 'successful login' });
+      }
+      return res.json({ status: 400, message: 'there is no such user in the system' });
+    } catch (e) {
+      res.json({ status: 501, message: e });
+    }
+  },
 };
--- src/api/user.api/user.router.js
+++ src/api/user.api/user.router.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 const {
-  signUp, signIn,
+  signUp, signIn, sendCheckCode,
 } = require('./user.controller');
 
 router.post('/signUp', signUp);
 router.post('/signIn', signIn);
-// router.post('/sendCheckCode', sendCheckCode);
+router.post('/sendCheckCode', sendCheckCode);
 module.exports = router;
