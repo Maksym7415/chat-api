57df2fcb2c27859418d893a9b54a3d4cefb958ca
Dolhopolov, Kyrylo <k.dolhopolov@telesens.ua>
2020-07-27 18:19:38 +0300

    add API to get message history of particular conversation

--- src/api/conversation.api/conversation.controller.js
+++ src/api/conversation.api/conversation.controller.js
@@ -1,12 +1,30 @@
 /* eslint-disable quotes */
 const createError = require('http-errors');
-const { sequelize, User } = require('../../../models');
+const {
+  sequelize,
+  User,
+  Conversation,
+  ChatUser,
+  Message, 
+  ChatMessage
+} = require('../../../models');
+const {
+  formErrorObject,
+  MAIN_ERROR_CODES
+} = require('../../../services/errorHandling');
+const { Op } = require("sequelize");
 
 module.exports = {
   getUserConversations: async (req, res, next) => {
-    const { userId } = req.token;
+    const {
+      userId
+    } = req.token;
     try {
-      const isUser = User.findOne({ where: { id: userId } });
+      const isUser = User.findOne({
+        where: {
+          id: userId
+        }
+      });
       if (isUser) {
         const queryString = `message.id as messageId,
             message.fkSenderId, message.message, 
@@ -26,16 +44,67 @@ module.exports = {
                 and user.id = ?
                 and message.id in
                     (select max(message.id) from message, chatmessage where message.id = fkMessageId and sendDate in 
-                        (select max(sendDate) from message, chatmessage where message.id = fkMessageId group by chatmessage.fkChatId) group by chatMessage.fkChatId);`,
-        {
+                        (select max(sendDate) from message, chatmessage where message.id = fkMessageId group by chatmessage.fkChatId) group by chatMessage.fkChatId);`, {
           type: sequelize.QueryTypes.SELECT,
-          replacements: [`${userId}`],
+          replacements: [userId],
         });
-        return res.json(userConversations);
+        res.json(userConversations);
       }
-      res.status(400).json('wrong user id');
+      next(createError(formErrorObject(MAIN_ERROR_CODES.NOT_EXISTS, 'User does not exist')));
+      // res.status(400).json('wrong user id');
     } catch (error) {
-      next(createError(501, error));
+      console.log(error);
+      next(createError(formErrorObject(MAIN_ERROR_CODES.UNHANDLED_ERROR)));
+      // next(createError(501, error));
     }
   },
+
+  conversationHistory: async (req, res, next) => {
+    const {
+      userId
+    } = req.token;
+    const conversationId = req.params.id;
+    try {
+      const user = await User.findOne({ where: { id: userId } });
+      const conversation = await Conversation.findOne({ where: { id: conversationId } });
+      if (!user) {
+        next(createError(formErrorObject(MAIN_ERROR_CODES.NOT_EXISTS, 'User does not exist')));
+      }
+      else if (!conversation) {
+        next(createError(formErrorObject(MAIN_ERROR_CODES.NOT_EXISTS, 'Conversation does not exist')));
+      } 
+      else {
+        const isUserConversation = await Conversation.findAll({
+          where: {
+            id: conversationId,
+          },
+          include: {
+            model: User,
+            through: {
+              model: ChatUser
+            },
+            where: {
+              id: userId
+            }
+          }
+        });
+        if (!isUserConversation.length) {
+          next(createError(formErrorObject(MAIN_ERROR_CODES.FORBIDDEN, 'User has not access to this conversation')));
+        } else {
+          const conversationHistory = await Message.findAll({
+            include:{
+              model: Conversation,
+              attributes: [],
+              where: {
+                id: conversationId,
+              }
+            }
+          });
+          res.json({data: conversationHistory});
+        }
+      }
+    } catch (error) {
+      next(createError(formErrorObject(MAIN_ERROR_CODES.UNHANDLED_ERROR)));
+    }
+  }
 };
--- src/api/conversation.api/conversation.router.js
+++ src/api/conversation.api/conversation.router.js
@@ -3,8 +3,11 @@ const authMiddleware = require('../middleware/auth');
 
 const {
   getUserConversations,
+  conversationHistory,
 } = require('./conversation.controller');
 
 router.get('/getUserConversations', authMiddleware, getUserConversations);
+
+router.get('/conversationHistory/:id', authMiddleware, conversationHistory);
 
 module.exports = router;
