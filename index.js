99967cc950446afb07bd176211740ce9ced95eb8
Maksym7415 <popovmaksim7415@gmail.com>
2020-08-10 13:02:33 +0300

    fixed index.js for hellium

--- index.js
+++ index.js
@@ -40,23 +40,9 @@ try {
 // const {
 //   app,
 //   http,
-//   // https
 // } = require('./server');
-// // const socketAppConnection = require('./socket.server');
-// // const crypto = require('crypto-js');
-// // const fs = require('fs');
-// // const http = require('http').createServer(app);
+
 // require('dotenv').config();
-
-// // socketAppConnection();
-// // const options = {
-// //   key: fs.readFileSync('keys/privatekey.pem').toString(),
-// //   cert: fs.readFileSync('keys/certificate.pem').toString()
-// // };
-
-// // https.createServer(options, app).listen(process.env.PORT, async () => {
-// //       console.log(`Listening on port ${process.env.PORT}`);
-// //   });
 
 // try {
 //   http.listen(socketPath, () => {
