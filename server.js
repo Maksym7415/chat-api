baa0414a16babafbef44f423d6af5215365bde44
Maksym7415 <popovmaksim7415@gmail.com>
2020-08-14 17:54:41 +0300

    moved socket connection from server.js to src/api/socket

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-plusplus */
 require('dotenv').config();
 const {
   errorHandling,
@@ -14,6 +13,7 @@ const http = require('http').createServer(app);
 // const https = require('https');
 // const io = require('socket.io')(https);
 const io = require('socket.io')(http);
+const initSocket = require('./src/api/socket');
 const getFileSize = require('./src/helpers/checkFileSize');
 const {
   ChatMessage, Message, User,
@@ -36,79 +36,8 @@ app.use('*', (req, res) => {
 });
 
 app.use(errorHandling);
-let file = null;
-io.on('connection', (socket) => {
-  let fileIterationsCount = {}; // creating object for counter of filePortion iterations
-  console.log('connection');
-  socket.on('chats', async ({ conversationId, message, userId }, successCallback) => { // successCallback to inform client about sucessfull sending of message
-    // if(message.type === 'file') {
 
-    // }
-    const newMessage = await Message.create({
-      message: message.message,
-      sendDate: message.sendDate,
-      messageType: message.messageType,
-      fkSenderId: message.fkSenderId,
-    });
-    await ChatMessage.create({
-      fkChatId: conversationId,
-      fkMessageId: newMessage.id,
-    });
-    const user = await User.findOne(
-      {
-        where: {
-          id: userId,
-        },
-      },
-    );
-    // const { Conversations: conversation } = await User.findOne(
-    //   {
-    //     where: {
-    //       id: userId,
-    //     },
-    //     include: {
-    //       model: Conversation,
-    //     },
-    //   },
-    // );
-    // conversation.forEach((el) => {
-    // io.emit(`userIdChat${el.id}`, message);
-    // });
-    io.emit(`userIdChat${conversationId}`, { ...message, User: user });
-    successCallback(true);
-  });
-  socket.on('files', ({
-    data, sendDate, messageType, userId, conversationId, fileSize, isUploaded, uniqueName, fileName, fileExtension, iterations,
-  }, successCallback) => {
-    fs.appendFile(`./uploads/${uniqueName}.${fileExtension}`, data, async (err) => {
-      if (err) return;
-      if (!fileIterationsCount[uniqueName]) {
-        fileIterationsCount[uniqueName] = 1;
-      } else fileIterationsCount[uniqueName]++;
-      if (iterations === fileIterationsCount[uniqueName]) { // checking if it's the last portion of file
-        const internalFileSize = getFilesizeInBytes(`./uploads/${uniqueName}.${fileExtension}`);
-        if (internalFileSize === fileSize) { // if we get not whole file we deleting it in other case we savin it in db
-          // const message = await Message.create({
-          //   message: id,
-          //   sendDate,
-          //   messageType,
-          //   fkSenderId,
-          // });
-          // await ChatMessage.create({
-          //   fkChatId: conversationId,
-          //   fkMessageId: message.id,
-          // });
-          io.emit(`userIdChat${conversationId}`, { ...message, User: user });
-          successCallback(true);
-        } else {
-          fs.unlink(`./uploads/${uniqueName}.${fileExtension}`, (err) => {
-            if (err) return;
-          });
-        }
-      }
-    });
-  });
-});
+initSocket(fs, io, Message, ChatMessage, User);
 
 module.exports = {
   app,
---
+++ src/api/socket/index.js
@@ -0,0 +1,93 @@
+module.exports = function initSocket(fs, io, Message, ChatMessage, User) {
+  io.on('connection', (socket) => {
+    let fileIterationsCount = {}; // creating object for counter of filePortion iterations
+    console.log('connection');
+    socket.on('chats', async ({ conversationId, message, userId }, successCallback) => { // successCallback to inform client about sucessfull sending of message
+      // if(message.type === 'file') {
+
+      // }
+      const newMessage = await Message.create({
+        message: message.message,
+        sendDate: message.sendDate,
+        messageType: message.messageType,
+        fkSenderId: message.fkSenderId,
+      });
+      await ChatMessage.create({
+        fkChatId: conversationId,
+        fkMessageId: newMessage.id,
+      });
+      const user = await User.findOne(
+        {
+          where: {
+            id: userId,
+          },
+        },
+      );
+      // const { Conversations: conversation } = await User.findOne(
+      //   {
+      //     where: {
+      //       id: userId,
+      //     },
+      //     include: {
+      //       model: Conversation,
+      //     },
+      //   },
+      // );
+      // conversation.forEach((el) => {
+      // io.emit(`userIdChat${el.id}`, message);
+      // });
+      io.emit(`userIdChat${conversationId}`, { ...message, User: user });
+      successCallback(true);
+    });
+    socket.on('files', ({
+      data, sendDate, messageType, userId, conversationId, fileSize, isUploaded, uniqueName, fileName, fileExtension, iterations,
+    }, successCallback) => {
+      fs.appendFile(`./uploads/${uniqueName}.${fileExtension}`, data, async (err) => {
+        if (err) return;
+        if (!fileIterationsCount[uniqueName]) {
+          fileIterationsCount[uniqueName] = 1;
+        } else fileIterationsCount[uniqueName]++;
+        if (iterations === fileIterationsCount[uniqueName]) { // checking if it's the last portion of file
+          const internalFileSize = getFilesizeInBytes(`./uploads/${uniqueName}.${fileExtension}`);
+          if (internalFileSize === fileSize) { // if we get not whole file we deleting it in other case we savin it in db
+            // const message = await Message.create({
+            //   message: id,
+            //   sendDate,
+            //   messageType,
+            //   fkSenderId,
+            // });
+            // await ChatMessage.create({
+            //   fkChatId: conversationId,
+            //   fkMessageId: message.id,
+            // });
+
+            // creating message
+            // const newMessage = await Message.create({
+            //   message: message.message,
+            //   sendDate: message.sendDate,
+            //   messageType: message.messageType,
+            //   fkSenderId: message.fkSenderId,
+            // });
+            // await ChatMessage.create({
+            //   fkChatId: conversationId,
+            //   fkMessageId: newMessage.id,
+            // });
+            // const user = await User.findOne(
+            //   {
+            //     where: {
+            //       id: userId,
+            //     },
+            //   },
+            // );
+            // io.emit(`userIdChat${conversationId}`, { ...message, User: user });
+            successCallback(true);
+          } else {
+            fs.unlink(`./uploads/${uniqueName}.${fileExtension}`, (err) => {
+              if (err) return;
+            });
+          }
+        }
+      });
+    });
+  });
+};
