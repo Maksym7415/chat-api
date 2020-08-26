068d545a0bfc4a284af00efadd90dd9b7d4747a6
Maksym7415 <popovmaksim7415@gmail.com>
2020-08-26 16:38:07 +0300

    finished with files upload

--- src/api/socket/index.js
+++ src/api/socket/index.js
@@ -1,22 +1,15 @@
 const fs = require('fs');
-const util = require('util');
 const {
   ChatMessage, Message, User, File,
 } = require('../../../models');
-const getFilesizeInBytes = require('../../helpers/checkFileSize');
+
+let filesAmount = {}; // object with key=userId where files count from one message
+let filesArray = {}; // object with key=userId of arrays of fileData from one message
 
 module.exports = function initSocket(io) {
   io.on('connection', (socket) => {
-    let fileIterationsCount = {}; // creating object for counter of filePortion iterations
-    let filesAmount = {}; // object with key=userId where files count from one message
-    let filesArray = {}; // object with key=userId of arrays of fileData from one message
-    const appendFileAsync = util.promisify(fs.appendFile);
     console.log('connection');
     socket.on('chats', async ({ conversationId, message, userId }, successCallback) => { // successCallback to inform client about sucessfull sending of message
-      // if(message.type === 'file') {
-
-      // }
-      console.log(message);
       if (!message) return successCallback(false);
       try {
         const newMessage = await Message.create({
@@ -45,36 +38,43 @@ module.exports = function initSocket(io) {
       }
     });
     socket.on('files', async ({
-      data, conversationId, fileSize, uniqueName, fileName, fileExtension, iterations, message, userId, isImage, filesCount,
+      data, conversationId, fileSize, uniqueName, fileName, fileExtension, message, userId, isImage, filesCount, messageId, sendDate,
     }, successCallback) => {
-      fs.appendFile(`./uploads/${uniqueName}.${fileExtension}`, data, async (err) => {
-        if (err) return;
+      if (!data) {
+        try {
+          const newMessage = await Message.create({
+            message: message.message,
+            sendDate: message.sendDate,
+            messageType: message.messageType,
+            fkSenderId: message.fkSenderId,
+          });
+          await ChatMessage.create({
+            fkChatId: conversationId,
+            fkMessageId: newMessage.id,
+          });
+          successCallback(newMessage.id);
+        } catch (error) {
+          successCallback('error');
+        }
+      } else {
+        fs.appendFile(`./uploads/${uniqueName}.${fileExtension}`, data, async (err) => {
+          if (err) return;
 
-        if (!fileIterationsCount[uniqueName]) {
-          fileIterationsCount[uniqueName] = 1;
-        } else fileIterationsCount[uniqueName]++; // creating object of iterations by unique name of file to control if whole file parts received from client
-
-        if (iterations === fileIterationsCount[uniqueName]) { // checking if it's the last portion of file
-          // setting count +1 and adding fileData object in filesArray when got whole file
           if (!filesAmount[userId]) filesAmount[userId] = 1;
           else filesAmount[userId]++;
           if (!filesArray[userId]) filesArray[userId] = [{ isImage, name: `${uniqueName}.${fileExtension}` }];
           else filesArray[userId] = [...filesArray[userId], { isImage, name: `${uniqueName}.${fileExtension}` }];
 
-          const internalFileSize = getFilesizeInBytes(`./uploads/${uniqueName}.${fileExtension}`);
-          if (internalFileSize === fileSize) { // if we get not whole file we deleting it in other case we savin it in db
-            // creating message
-            try {
-              const newMessage = await Message.create({
-                message: message.message,
-                sendDate: message.sendDate,
-                messageType: message.messageType,
-                fkSenderId: message.fkSenderId,
-              });
-              await ChatMessage.create({
-                fkChatId: conversationId,
-                fkMessageId: newMessage.id,
-              });
+          try {
+            await File.create({
+              fileStorageName: uniqueName,
+              fileUserName: fileName,
+              size: fileSize,
+              extension: fileExtension,
+              fkMessageId: messageId,
+            });
+
+            if (filesAmount[userId] === filesCount) {
               const user = await User.findOne(
                 {
                   where: {
@@ -82,34 +82,25 @@ module.exports = function initSocket(io) {
                   },
                 },
               );
-              await File.create({
-                fileStorageName: uniqueName,
-                fileUserName: fileName,
-                size: fileSize,
-                extension: fileExtension,
-                fkMessageId: newMessage.id,
+              successCallback('upload done');
+              let requestMessage = {
+                message,
+                fkSenderId: userId,
+                sendDate,
+                messageType: 'File',
+              };
+              io.emit(`userIdChat${conversationId}`, {
+                ...requestMessage, id: messageId, fileData: filesArray[userId], User: user.dataValues,
               });
-
-              console.log(internalFileSize === fileSize);
-
-              if (filesAmount[userId] === filesCount) {
-                filesAmount[userId] = 0;
-                successCallback(true);
-                io.emit(`userIdChat${conversationId}`, {
-                  ...message, id: newMessage.id, fileData: filesArray[userId], User: user.dataValues,
-                });
-                filesArray[userId] = [];
-              }
-            } catch (error) {
-              successCallback(false);
+              filesAmount[userId] = 0;
+              filesArray[userId] = [];
             }
-          } else {
-            fs.unlink(`./uploads/${uniqueName}.${fileExtension}`, (err) => {
-              if (err) return;
-            });
+            successCallback('file added');
+          } catch (error) {
+            successCallback(error);
           }
-        }
-      });
+        });
+      }
     });
   });
 };
---
+++ src/helpers/dive.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-restricted-syntax */
+module.exports = (strings, ...values) => {
+  let obj = values[0];
+  for (const key of strings[1].split('.')) {
+    if (obj && key && typeof obj === 'object') {
+      obj = obj[key];
+    } else return undefined;
+  }
+  return obj;
+};
