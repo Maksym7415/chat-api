267d87564332c45340968cff93a9bb78e6fa4630
Maksym7415 <popovmaksim7415@gmail.com>
2020-09-10 16:22:43 +0300

    finished find menu link function

--- server.js
+++ server.js
@@ -33,99 +33,73 @@ initSocket(io);
 //
 //
 //
-const vgmUrl = 'https://www.telegraafhotel.com/et/restoran-tallinnas/';
 
-let array = [];
-let exit;
-let result;
-let count = 0;
+async function getMenuHtml(url) {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const vgmUrl = 'https://www.telegraafhotel.com/et/restoran-tallinnas/';
+const errorLink = 'https://github.com';
+const firstStageUrl = 'https://www.telegraafhotel.com/et/restoran-tallinnas/restoran-tchaikovsky-menuu/';
+
+let entriesCounter = 0; // indicates deps of links stages
+let nodesArray = []; // array of links in each stage
+let targetHref = ''; // linl with menu
+let isParsing = false; // indicates that link was found and started parsing process to prevent recurseve parsing
+
+let resolve = () => console.log('init'); // resolving promise when reaches the last interation in array
+let promiseCallback = (res) => () => res(); // saving promise in clousere to resolve it later
+let promise = () => new Promise((res, rej) => {
+  resolve = promiseCallback(res);
+}); // promise for awaiting iteration process in nodesArray on each stage
 
 async function getMenu(url, tag) {
-  if (result) return;
-  count += 1;
-  // console.log(count);
+  if (targetHref) return;
   try {
-    const response = await got(url);
-    const $ = cheerio.load(response.body);
-    let nodesArray = [];
-    $(tag).each((i, link) => {
-      const { href } = link.attribs;
-      nodesArray.push({
-        node: link.childNodes,
-        href,
-      });
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    $(tag).each((i, element) => {
+      if (targetHref) return;
+      const { href } = element.attribs;
+      if (!href) return;
+      if (href.split('/')[2] !== url.split('/')[2]) return;
+      const targetLink = element.children && element.children && element.children[0] && element.children[0].data;
+      if (targetLink === 'Broneeri hoolitsus') {
+        return targetHref = href;
+      }
+      nodesArray.push(href);
     });
-    nodesArray.forEach((el) => {
-      console.log(el.node[0].data);
-      if (el.node[0].data === 'Please click here to see the cocktail menu!') {
-        result = el.href;
-      } else getMenu(el.href, 'a');
-      // console.log(href);
+    if (isParsing) return;
+    // console.log(targetHref, entriesCounter);
+    if (targetHref) {
+      isParsing = true;
+      return console.log('I am ready to parse');
+    }
+    console.log(entriesCounter);
+    if (entriesCounter >= 3) return; // (() => console.log('Nothing was found'))();
+    entriesCounter++;
+    if (entriesCounter > 1) await promise();
+    if (isParsing) return;
+    nodesArray.forEach((url, i) => {
+      // console.log(i, nodesArray.length);
+      getMenu(url, 'a');
+      if (i === nodesArray.length - 1) {
+        nodesArray = [];
+        resolve();
+      }
     });
+    return;
   } catch (error) {
     console.log(error);
   }
 }
 
 getMenu(vgmUrl, 'a');
-
-// got(vgmUrl).then((response) => {
-//   let $ = cheerio.load(response.body);
-//   $('a').each((i, link) => {
-//     const { href } = link.attribs;
-//     if (href && href.includes('menuu')) {
-//       console.log(href);
-//       axios.get(href, { responseType: 'text' }).then((res) => {
-//         // console.log(res);
-//         let $ = cheerio.load(res, { decodeEntities: false });
-//         $('div').each((i, link) => {
-//           console.log('hello');
-//           const divs = link.html();
-//           if (divs) console.log(divs);
-//         });
-//         // fs.writeFile('./uploads/index.html', result.html(), () => {});
-//       });
-//     }
-//   });
-// }).catch((err) => {
-//   console.log(err);
-// });
-
-// let TextElem = (e) => ({
-//   toJSON: () => ({
-//     type:
-//       'TextElem',
-//     textContent:
-//       e.textContent,
-//   }),
-// });
-
-// let Elem = (e) => ({
-//   toJSON: () => ({
-//     type:
-//       'Elem',
-//     tagName:
-//       e.tagName,
-//     attributes:
-//       Array.from(e.attributes, ({ name, value }) => [name, value]),
-//     children:
-//       Array.from(e.childNodes, fromNode),
-//   }),
-// });
-
-// // fromNode :: Node -> Elem
-// var fromNode = (e) => {
-//   switch (e.nodeType) {
-//     case 3: return TextElem(e);
-//     default: return Elem(e);
-//   }
-// };
-
-// // html2json :: Node -> JSONString
-// let html2json = (e) => JSON.stringify(Elem(e), null, '  ');
-
-// let mainDiv = document.getElementsByClassName('content-formatted');
-// html2json(mainDiv[1]);
 //
 //
 //
