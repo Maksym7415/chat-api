5017d6e3775ed4044d95b2df29a4959e6e8aa65e
vit9 <vit91112@gmail.com>
2020-09-14 11:43:44 +0300

    add message editing and deleting

--- src/api/socket/index.js
+++ src/api/socket/index.js
@@ -11,16 +11,18 @@ module.exports = function initSocket(io) {
   io.on('connection', (socket) => {
     console.log('connection');
     socket.on('chats', async ({
-      conversationId, message, userId, opponentId,
+      conversationId, message, userId, opponentId, messageId, isDeleteMessage,
     }, successCallback) => { // successCallback to inform client about sucessfull sending of message
       if (!message) return successCallback(false);
+      let newMessage = {};
+      let user = {};
       try {
+        user = await User.findOne({
+          where: {
+            id: userId,
+          },
+        });
         if (!conversationId) {
-          const user = await User.findOne({
-            where: {
-              id: userId,
-            },
-          });
           const opponent = await User.findOne({
             where: {
               id: opponentId,
@@ -30,23 +32,44 @@ module.exports = function initSocket(io) {
           io.emit(`userIdNewChat${userId}`, { ...newMessage, User: user }, newConversationId);
           return io.emit(`userIdNewChat${opponentId}`, { ...newMessage, User: user }, newConversationId);
         }
-        const newMessage = await Message.create({
-          message: message.message,
-          sendDate: message.sendDate,
-          messageType: message.messageType,
-          fkSenderId: message.fkSenderId,
-        });
-        await ChatMessage.create({
-          fkChatId: conversationId,
-          fkMessageId: newMessage.id,
-        });
-        const user = await User.findOne(
-          {
+        if (messageId) {
+          newMessage = await Message.update({
+            message: message.message,
+            sendDate: message.sendDate,
+          }, {
             where: {
-              id: userId,
+              id: message.fkSenderId,
             },
-          },
-        );
+          });
+        } else if (isDeleteMessage) {
+          await ChatMessage.delete({
+            where: {
+              fkChatId: conversationId,
+              fkMessageId: messageId,
+            },
+          });
+          await File.delet({
+            where: {
+              fkMessageId: messageId,
+            },
+          });
+          await Message.delete({
+            where: {
+              id: message.fkSenderId,
+            },
+          });
+        } else {
+          newMessage = await Message.create({
+            message: message.message,
+            sendDate: message.sendDate,
+            messageType: message.messageType,
+            fkSenderId: message.fkSenderId,
+          });
+          await ChatMessage.create({
+            fkChatId: conversationId,
+            fkMessageId: newMessage.id,
+          });
+        }
         io.emit(`userIdChat${conversationId}`, {
           ...message, id: newMessage.id, Files: [], User: user,
         });
