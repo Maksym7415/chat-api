0e0cac9b7686a3088a3201bdbb721642cc6cd7b5
vit9 <vit91112@gmail.com>
2020-09-23 12:09:03 +0300

    delete comments

--- index.js
+++ index.js
@@ -3,21 +3,9 @@ const {
   http,
   // https
 } = require('./server');
-// const socketAppConnection = require('./socket.server');
-// const crypto = require('crypto-js');
-// const fs = require('fs');
-// const http = require('http').createServer(app);
+
 require('dotenv').config();
 
-// socketAppConnection();
-// const options = {
-//   key: fs.readFileSync('keys/privatekey.pem').toString(),
-//   cert: fs.readFileSync('keys/certificate.pem').toString()
-// };
-
-// https.createServer(options, app).listen(process.env.PORT, async () => {
-//       console.log(`Listening on port ${process.env.PORT}`);
-//   });
 
 try {
   http.listen(process.env.PORT, async () => {
@@ -29,27 +17,3 @@ try {
   http.close();
 }
 
-// THIS CODE FOR SERVER
-
-// const fs = require('fs');
-
-// let subdomain = 'test'
-
-// let socketPath = `/home/asmer/node_hosts/${subdomain}` //путь к сокету
-
-// const {
-//   app,
-//   http,
-// } = require('./server');
-
-// require('dotenv').config();
-
-// try {
-//   http.listen(socketPath, () => {
-//     console.log(`Now listening on ${socketPath}`)
-//     fs.chmodSync(socketPath, '777'); //права доступа к файлу сокета
-//   });
-// } catch (error) {
-//   console.log(error);
-//   http.close();
-// }
